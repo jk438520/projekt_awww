20e9fcd75e8cf401b450aff9ad6ad37c370fc6f3
jk438520 <jk438520@students.mimuw.edu.pl>
2023-03-13 19:28:21 +0100

    mobile layout

---
+++ mobile.js
@@ -0,0 +1,8 @@
+function openTab(tabClass, tabID) {
+    var i;
+    var x = document.getElementsByClassName(tabClass);
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabID).style.display = "block";
+}
